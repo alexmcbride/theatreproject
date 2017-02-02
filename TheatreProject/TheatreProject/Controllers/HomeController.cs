dc025fa8ef0a1619ef96f4f25631d0ce92a61dad
Alex McBride <xandermcbride@gmail.com>
2017-02-02 21:18:39 +0000

    Removed about page.

--- TheatreProject/TheatreProject/Controllers/HomeController.cs
+++ TheatreProject/TheatreProject/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using TheatreProject.Models;
 
 namespace TheatreProject.Controllers
@@ -14,21 +10,13 @@ namespace TheatreProject.Controllers
         {
             var categories = new Category[]
             {
-                //new Category { CategoryId=1, Name="Announcements" },
-                //new Category { CategoryId=2, Name="News" },
-                //new Category { CategoryId=3, Name="Movie Reviews" },
-                //new Category { CategoryId=4, Name="Theatre Reviews" },
+                new Category { CategoryId=1, Name="Announcements" },
+                new Category { CategoryId=2, Name="News" },
+                new Category { CategoryId=3, Name="Movie Reviews" },
+                new Category { CategoryId=4, Name="Theatre Reviews" },
             };
 
             return View(categories);
-        }
-
-        [AllowAnonymous]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
         }
 
         [AllowAnonymous]
