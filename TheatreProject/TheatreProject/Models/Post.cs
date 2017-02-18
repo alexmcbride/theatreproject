28a66605f1a855a640fb582936750e7ad2b54e94
Alex McBride <xandermcbride@gmail.com>
2017-02-18 12:31:55 +0000

    Improvements to comments link.

--- TheatreProject/TheatreProject/Models/Post.cs
+++ TheatreProject/TheatreProject/Models/Post.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace TheatreProject.Models
 {
@@ -31,6 +32,16 @@ namespace TheatreProject.Models
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual Staff Staff { get; set; }
         public virtual Category Category { get; set; }
+
+        public bool HasApproveComments
+        {
+            get { return Comments.Any(c => c.IsApproved); }
+        }
+
+        public int ApproveCommentCount
+        {
+            get { return Comments.Where(c => c.IsApproved).Count(); }
+        }
 
         public bool BelongsTo(string userId)
         {
